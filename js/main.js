5754f49f06fc92545087a7b000e00abfd2bd0181
Ng Guoyou <fauxface+github@gmail.com>
2013-11-04 14:12:52 +0800

    Require.js dependency loading

--- js/main.js
+++ js/main.js
@@ -1,24 +1,39 @@
-require([
-  "vendor/three/three.min",
-  "vendor/three/CopyShader",
-  "vendor/three/MaskPass",
-  "vendor/three/EffectComposer",
-  "vendor/three/KaleidoShader",
-  "vendor/three/RenderPass",
-  "vendor/three/RGBShiftShader",
-  "vendor/three/ShaderPass",
-  "vendor/jquery-2.0.3.min",
-  "vendor/keypress-1.0.8.min",
-  "lib/Scaffold",
-  "lib/background",
-  "lib/player",
-  "lib/item",
-  "lib/cell",
-  "lib/game"], function () {
-  'use strict';
+require.config({
+  shim: {
+    'vendor/three/EffectComposer': {
+      deps: ['vendor/three/CopyShader']
+    },
+    'lib/background': {
+        deps: ['lib/Scaffold']
+    },
+    'lib/game': {
+        deps: ['lib/player', 'lib/item', 'lib/cell']
+    }
+  }
+});
 
-  $(document).ready(function () {
-    new Game();
-    new Background('background');
+require(["vendor/three/three.min"], function () {
+  require([
+    "vendor/three/CopyShader",
+    "vendor/three/MaskPass",
+    "vendor/three/EffectComposer",
+    "vendor/three/KaleidoShader",
+    "vendor/three/RenderPass",
+    "vendor/three/RGBShiftShader",
+    "vendor/three/ShaderPass",
+    "vendor/jquery-2.0.3.min",
+    "vendor/keypress-1.0.8.min",
+    "lib/Scaffold",
+    "lib/background",
+    "lib/player",
+    "lib/item",
+    "lib/cell",
+    "lib/game"], function () {
+    'use strict';
+
+    $(document).ready(function () {
+      new Game();
+      new Background('background');
+    });
   });
 });
